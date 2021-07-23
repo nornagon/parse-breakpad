a60aaf27509f2944d733339f9b9b89b78a234b50
Jeremy Rose <nornagon@nornagon.net>
2021-07-22 17:37:37 -0700

    faster parsing

--- index.js
+++ index.js
@@ -21,18 +21,12 @@ async function parse(readable) {
   })
 
   const functionIntervals = binaryTree.newTree()
-  const lineIntervals = binaryTree.newTree()
   const publicAddresses = binaryTree.newTree()
 
   const symbolFile = {
     functions: functionIntervals,
-    lines: lineIntervals,
     publicAddresses: publicAddresses,
     lookup(offset) {
-      const line = binaryTree.lb(this.lines, offset)
-      if (line && offset < line.address + line.size) {
-        return line
-      }
       const func = binaryTree.lb(this.functions, offset)
       if (func && offset < func.address + func.size) {
         return { func }
@@ -49,7 +43,16 @@ async function parse(readable) {
   let lastFunc = null
   for await (const line of rl) {
     let m
-    if (m = /^MODULE (\S+) (\S+) (\S+) (.+)$/.exec(line)) {
+    if (m = /^([0-9a-f]+) ([0-9a-f]+) (\d+) (\d+)$/.exec(line)) { // this is first because it's the most common kind
+      if (lastFunc) {
+        const [, addressHex, sizeHex, lineNumber, fileId] = m
+        const address = parseInt(addressHex, 16)
+        const size = parseInt(sizeHex, 16)
+        const file = filesById.get(fileId)
+        const line = parseInt(lineNumber, 10)
+        lastFunc.lines.push({address, size, file, line})
+      }
+    } else if (m = /^MODULE (\S+) (\S+) (\S+) (.+)$/.exec(line)) {
       const [, os, arch, id, name] = m
       symbolFile.module = { os, arch, id, name }
     } else if (m = /^FILE (\d+) (.+)$/.exec(line)) {
@@ -60,7 +63,7 @@ async function parse(readable) {
       const address = parseInt(addressHex, 16)
       const size = parseInt(sizeHex, 16)
       const parameterSize = parseInt(parameterSizeHex, 16)
-      const func = { address, size, parameterSize, name, multiple: !!multiple }
+      const func = { address, size, parameterSize, name, multiple: !!multiple, lines: [] }
       binaryTree.insert(functionIntervals, func)
       lastFunc = func
     } else if (m = /^PUBLIC (?:(m) )?([0-9a-f]+) ([0-9a-f]+) (.+)$/.exec(line)) {
@@ -69,13 +72,6 @@ async function parse(readable) {
       const parameterSize = parseInt(parameterSizeHex, 16)
       const func = { address, parameterSize, name, multiple: !!multiple }
       binaryTree.insert(publicAddresses, func)
-    } else if (m = /^([0-9a-f]+) ([0-9a-f]+) (\d+) (\d+)$/.exec(line)) {
-      const [, addressHex, sizeHex, lineNumber, fileId] = m
-      const address = parseInt(addressHex, 16)
-      const size = parseInt(sizeHex, 16)
-      const file = filesById.get(fileId)
-      const line = parseInt(lineNumber, 10)
-      binaryTree.insert(lineIntervals, { address, size, file, line, func: lastFunc })
     } else {
       // There are some other records (STACK WIN, STACK CFI) that can
       // occur in breakpad symbol files, but we ignore them for now.
